a015a38c1984ca1ae0aa2c5e681d6923c54a7dde
Alok Kumar Rathod <alok@Aloks-MacBook-Air.local>
2025-07-19 12:19:42 +0530

    url's updated

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,7 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/job";
+export const USER_API_END_POINT =
+  "https://job-portal-ed57.onrender.com/api/user";
+export const JOB_API_END_POINT = "https://job-portal-ed57.onrender.com/api/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/company";
+  "https://job-portal-ed57.onrender.com/api/application";
+export const COMPANY_API_END_POINT =
+  "https://job-portal-ed57.onrender.com/api/company";
