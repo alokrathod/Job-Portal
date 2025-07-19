39e7a4ff28a7df225c71401d3b2ebc68276ecc1d
Alok Kumar Rathod <alok@Aloks-MacBook-Air.local>
2025-07-19 11:59:03 +0530

    production ready

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import connectDB from "./utils/db.js";
 import userRouter from "./routes/user.route.js";
@@ -11,6 +12,8 @@ import jobRouter from "./routes/job.route.js";
 import applicationRouter from "./routes/application.route.js";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json());
@@ -27,6 +30,11 @@ app.use("/api/user", userRouter);
 app.use("/api/company", companyRouter);
 app.use("/api/job", jobRouter);
 app.use("/api/application", applicationRouter);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
